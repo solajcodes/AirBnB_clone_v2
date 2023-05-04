d4bb26cea4d2e0988fab1bdb6862078d3fb4ac0f
solajcodes <olalekansunday638@gmail.com>
2023-05-04 14:00:11 +0000

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,7 @@
 apt-get -y update
 apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-echo -e "Hello There!\nGeneral Kenobi!" | tee /data/web_static/releases/test/index.html
+echo -e "Hello Friends!\nThe saviour is here" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "s.^\tlocation / {.\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\n\tlocation / {." /etc/nginx/sites-available/default
