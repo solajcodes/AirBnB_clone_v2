d63cde3f87273d99f2f016b6afc04c14f3fec15a
solajcodes <olalekansunday638@gmail.com>
2023-05-04 18:10:18 +0100

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-
-""" Packs the web_static to a tar """
-
 from fabric.api import local
-from datetime import datetime
+from time import strftime
+from datetime import date
 
 
 def do_pack():
-    """ A function to pack the current webstatic version """
+    """ A script that generates archive the contents of web_static folder"""
 
-    local('mkdir -p versions')
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
 
-    current = datetime.now()
-    filename = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        current.year, current.month, current.day,
-        current.hour, current.minute, current.second)
+        return "versions/web_static_{}.tgz".format(filename)
 
-    path = local('tar -cvzf {} web_static'.format(filename))
-    if path.succeeded:
-        print("Packed webstatic to {}".format(filename))
-        return filename
-    else:
+    except Exception as e:
         return None
